d2a4b6659ee33d20462c7dc21ad2e5911581fefe
m a r i e 2 :) <mbremner@andrew.cmu.edu>
2018-04-25 16:24:50 -0400

    ble string instead of int array for pressure map

--- sensor-team/sensor-team.ino
+++ sensor-team/sensor-team.ino
@@ -7,57 +7,126 @@
   #include <SoftwareSerial.h>
 #endif
 
-/*=========================================================================
-    APPLICATION SETTINGS
 
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
-/*=========================================================================*/
+/*===============HELPER FUNCTION DECLARATIONS=====================*/
+void error(const __FlashStringHelper*err);
+void displaySensorDetails(Adafruit_BNO055 b);
+void connect_to_ble();
+void initialize_bnos();
+float get_imu_angle();
+void int_array_to_string(int &int_arr, char &str);
+/*===============HELPER FUNCTION DECLARATIONS=====================*/
+
 
 /*==========================GLOBALS========================================*/
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
                              BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
                              BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-Adafruit_BNO055 bno = Adafruit_BNO055();
+Adafruit_BNO055 bno1 = Adafruit_BNO055(55, 0x28);
+Adafruit_BNO055 bno2 = Adafruit_BNO055(56, 0x29);
 int packet_id = 0;
 const int ble_buffer_size = 15;
+const int pressure_size = 105;
+int pressure_map_arr[105];
+char pressure_char_arr[105];
 /*==========================GLOBALS========================================*/
 
-// A small helper
+
+void setup(void)
+{
+  while(!Serial);
+  Serial.begin(9600);
+  
+  Serial.println(F("Sensor Team IMU->BLE"));
+  Serial.println(F("------------------------------------------------"));
+  
+  connect_to_ble();
+
+  initialize_bnos();
+}
+
+
+
+void loop(void)
+{
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  //JsonArray& pressure_array = root.createNestedArray("pressure_array");
+  char json_buf[1000];
+  int buf_len;
+  unsigned long tick, tock;
+
+  tick = millis();
+  
+  
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+    Serial.println("waiting for connection...");
+  }
+
+  /* signal the beginning of this packet and flush the pipeline */
+  ble.print("*");
+  ble.flush();
+
+  /* get pressure points and map to a string */
+  get_pressure_array(pressure_map_arr);
+  int_array_to_string(pressure_map_arr, pressure_char_arr);
+
+  /* construct json object */
+  root["id"] = packet_id++;
+  root["imu_angle"] = get_imu_angle();
+  root["temp"] = 0;
+  root["hum"] = 0;
+  root["pressure_array"] = pressure_char_arr;
+
+  /* stringify json */
+  root.printTo(json_buf);
+  buf_len = strlen(json_buf);
+  Serial.println(json_buf);
+
+  /* break up string into chunks of ble_buffer_size and send individually */
+  for (int i = 0; i < buf_len; i += ble_buffer_size){
+      char temp_buf[250];
+      
+      memcpy(temp_buf, json_buf+i, ble_buffer_size);
+      temp_buf[ble_buffer_size] = '\0';
+      
+      Serial.println(temp_buf);
+       
+      /* Send input data to host via Bluefruit */
+      ble.print(temp_buf);
+      ble.flush();
+      delay(1000);
+  }
+
+   tock = millis();
+   Serial.print("This iteration took ");
+   Serial.print(tock - tick);
+   Serial.println(" milliseconds");
+}
+
+
+/** =========================== HELPER FUNCTIONS ============================ **/
 void error(const __FlashStringHelper*err) {
   Serial.println(err);
   while (1);
 }
 
+void displaySensorDetails(Adafruit_BNO055 b)
+{
+  sensor_t sensor;
+  b.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
 
 void connect_to_ble(){
   Serial.println(F("******************************"));
@@ -101,95 +170,43 @@ void connect_to_ble(){
   Serial.println(F("******************************"));
 }
 
-void setup(void)
-{
-  while(!Serial);
-
-  Serial.begin(9600);
-  Serial.println(F("Sensor Team IMU->BLE"));
-  Serial.println(F("------------------------------------------------"));
-  
-  connect_to_ble();
-  
-    /* Initialise the sensor */
-  if(!bno.begin()) {
+void initialize_bnos(){
+  /* Initialise the sensor */
+  if(!bno1.begin()) {
     /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    //while(1);
+    Serial.print("Ooops, failed to connect bno1 ... Check your wiring or I2C ADDR!");
+  }
+
+  /* Initialise the sensor */
+  if(!bno2.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, failed to connect bno2 ... Check your wiring or I2C ADDR!");
   }
 
   delay(1000);
-
-  /* Display the current temperature */
-  int8_t temp = bno.getTemp();
-  Serial.print("Current Temperature: ");
-  Serial.print(temp);
-  Serial.println(" C");
-  Serial.println("");
-
-  bno.setExtCrystalUse(true);
-
-  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
-}
-
-
-void insert_imu_data(JsonObject& imu1, JsonObject& imu2){
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  imu1["imu_id"] = 1;
-  imu1["x"] = 0.0;
-  imu1["y"] = 0.0;
-  imu1["z"] = 0.0;
-
-  imu2["imu_id"] = 2;
-  imu2["x"] = 0.0;
-  imu2["y"] = 0.0;
-  imu2["z"] = 0.0;
-}
-
-
-void loop(void)
-{
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  JsonObject& imu1 = jsonBuffer.createObject();
-  JsonObject& imu2 = jsonBuffer.createObject();
-  JsonArray& imu_array = root.createNestedArray("imu");
-  char json_buf[500];
-  int buf_len;
   
-
-  /* Wait for connection */
-  while (! ble.isConnected()) {
-  }
-
-  /* signal the beginning of this packet and flush the pipeline */
-  ble.print("*");
-  ble.flush();
-
-  /* construct json object */
-  root["id"] = packet_id++;
-  insert_imu_data(imu1, imu2);
-  imu_array.add(imu1);
-  imu_array.add(imu2);
-  root["temp"] = 0;
-  root["hum"] = 0;
-
-  /* stringify json */
-  root.printTo(json_buf);
-  buf_len = strlen(json_buf);
-
-  for (int i = 0; i < buf_len; i += ble_buffer_size){
-      char temp_buf[250];
-      
-      memcpy(temp_buf, json_buf+i, ble_buffer_size);
-      temp_buf[ble_buffer_size] = '\0';
-      
-      Serial.println(temp_buf);
-       
-      /* Send input data to host via Bluefruit */
-      ble.print(temp_buf);
-      ble.flush();
-  }
-
-  delay(1000);
+  bno1.setExtCrystalUse(true);
+  bno2.setExtCrystalUse(true);
 }
+
+float get_imu_angle(){
+  sensors_event_t event1, event2;
+  bno1.getEvent(&event1);
+  bno2.getEvent(&event2);
+  return ((float)event1.orientation.z + (float)event2.orientation.z) / 2.0;
+}
+
+// str must be pressure_size bytes 
+void int_array_to_string(int *int_arr, char *char_arr){
+  for (int i = 0; i < pressure_size; i++){
+    char_arr[i] = (char)int_arr[i];
+  }
+}
+
+void get_pressure_array(int *A){
+  for (int i = 0; i < pressure_size; i++){
+    pressure_map_arr[i] = i % 100 + 48;
+  }
+}
+
+
